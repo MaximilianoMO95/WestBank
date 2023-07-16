4a31b943748ec0c7f5566b3146f1aec610afc217
MaximilianoMO95 <131606838+MaximilianoMO95@users.noreply.github.com>
2023-07-16 00:11:06 -0400

    Update NavBarView.java

--- westbank/app/src/main/java/westbank/interfaz/NavBarView.java
+++ westbank/app/src/main/java/westbank/interfaz/NavBarView.java
@@ -5,67 +5,67 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class NavBarView extends JMenuBar {
-        private CustomJMenuItem home = new CustomJMenuItem("Inicio");
-        private CustomJMenuItem exit = new CustomJMenuItem("Salir");
+    private CustomJMenuItem home = new CustomJMenuItem("Inicio");
+    private CustomJMenuItem exit = new CustomJMenuItem("Salir");
 
-        private CustomJMenuItem a1 = new CustomJMenuItem("Transferir");
-        private CustomJMenuItem a2 = new CustomJMenuItem("Historial Transferencias");
+    private CustomJMenuItem a1 = new CustomJMenuItem("Transferir");
+    private CustomJMenuItem a2 = new CustomJMenuItem("Historial Transferencias");
 
-        public NavBarView() {
-                setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+    public NavBarView() {
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 
-                add(home);
-                add(a1);
-                add(a2);
-                add(exit);
+        add(home);
+        add(a1);
+        add(a2);
+        add(exit);
 
-                setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        }
+        setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+    }
 
-        public void showHome(ActionListener actionListener) {
-                home.addActionListener(actionListener);
-        }
+    public void showHome(ActionListener actionListener) {
+        home.addActionListener(actionListener);
+    }
 
-        public void showTransferir(ActionListener actionListener) {
-                a1.addActionListener(actionListener);
-        }
+    public void showTransferir(ActionListener actionListener) {
+        a1.addActionListener(actionListener);
+    }
 
-        public void showHistorialTransferencias(ActionListener actionListener) {
-                a2.addActionListener(actionListener);
-        }
+    public void showHistorialTransferencias(ActionListener actionListener) {
+        a2.addActionListener(actionListener);
+    }
 
-        public void exitApp(ActionListener actionListener) {
-                exit.addActionListener(actionListener);
-        }
+    public void exitApp(ActionListener actionListener) {
+        exit.addActionListener(actionListener);
+    }
 }
 
 class CustomJMenu extends JMenu {
-        private static final int MENU_WIDTH = 200;
-        private static final int MENU_HEIGHT = 45;
+    private static final int MENU_WIDTH = 200;
+    private static final int MENU_HEIGHT = 45;
 
-        public CustomJMenu(String text) {
-                super(text);
-                setHorizontalAlignment(SwingConstants.CENTER);
-        }
-    
-        @Override
-        public Dimension getPreferredSize() {
-                return new Dimension(MENU_WIDTH, MENU_HEIGHT);
-        }
+    public CustomJMenu(String text) {
+        super(text);
+        setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(MENU_WIDTH, MENU_HEIGHT);
+    }
 }
 
 class CustomJMenuItem extends JMenuItem {
-        private static final int ITEM_WIDTH = 200;
-        private static final int ITEM_HEIGHT = 45;
-    
-        public CustomJMenuItem(String text) {
-                super(text);
-                setHorizontalAlignment(SwingConstants.CENTER);
-                setFont(getFont().deriveFont(Font.BOLD));
-        }
+    private static final int ITEM_WIDTH = 200;
+    private static final int ITEM_HEIGHT = 45;
 
-        @Override
-        public Dimension getPreferredSize() {
-                return new Dimension(ITEM_WIDTH, ITEM_HEIGHT);
-        }
+    public CustomJMenuItem(String text) {
+        super(text);
+        setHorizontalAlignment(SwingConstants.CENTER);
+        setFont(getFont().deriveFont(Font.BOLD));
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(ITEM_WIDTH, ITEM_HEIGHT);
+    }
 }
