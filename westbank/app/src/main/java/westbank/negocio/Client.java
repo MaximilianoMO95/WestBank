32d4a4cce71b17028eafdd860498d22955fa2c51
MaximilianoMO95 <131606838+MaximilianoMO95@users.noreply.github.com>
2023-07-16 00:35:35 -0400

    Update Client.java

--- westbank/app/src/main/java/westbank/negocio/Client.java
+++ westbank/app/src/main/java/westbank/negocio/Client.java
@@ -4,6 +4,7 @@ public class Client {
         public int id;
         public String run;
         public String dv;
+        public String clave;
         public String name;
         public String ap_paterno;
         public String ap_materno;
@@ -12,9 +13,10 @@ public class Client {
         public String tel;
         private Account account;
 
-        public Client(String run, String dv, String name, String ap_paterno, String ap_materno, String address, String comuna, String tel, Account account) {
+        public Client(String run, String dv, String clave, String name, String ap_paterno, String ap_materno, String address, String comuna, String tel, Account account) {
                 this.run        = run;
                 this.dv         = dv;
+                this.clave      = clave;
                 this.name       = name;
                 this.ap_paterno = ap_paterno;
                 this.ap_materno = ap_materno;
@@ -24,10 +26,11 @@ public class Client {
                 this.account    = account;
         }
 
-        public Client(int id, String run, String dv, String name, String ap_paterno, String ap_materno, String address, String comuna, String tel, Account account) {
+        public Client(int id, String run, String dv, String clave, String name, String ap_paterno, String ap_materno, String address, String comuna, String tel, Account account) {
                 this.id         = id;
                 this.run        = run;
                 this.dv         = dv;
+                this.clave      = clave;
                 this.name       = name;
                 this.ap_paterno = ap_paterno;
                 this.ap_materno = ap_materno;
@@ -40,4 +43,7 @@ public class Client {
         public Account getAccount() {
                 return  this.account;
         }
+        public String getName() {
+        return name;
+    }
 }
