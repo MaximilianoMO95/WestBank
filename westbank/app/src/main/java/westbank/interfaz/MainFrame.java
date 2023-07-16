2a4e1378ea82731bb0b1ad599e04e8e3100902e0
MaximilianoMO95 <131606838+MaximilianoMO95@users.noreply.github.com>
2023-07-16 00:10:26 -0400

    Update MainFrame.java

--- westbank/app/src/main/java/westbank/interfaz/MainFrame.java
+++ westbank/app/src/main/java/westbank/interfaz/MainFrame.java
@@ -3,44 +3,68 @@ package westbank.interfaz;
 import westbank.acceso.*;
 import java.awt.*;
 import javax.swing.*;
+import westbank.negocio.Client;
 
 public class MainFrame extends JFrame {
-        private static final int WINDOW_WIDTH = 1200;
-        private static final int WINDOW_HEIGHT = 700;
+    private static final int WINDOW_WIDTH = 1200;
+    private static final int WINDOW_HEIGHT = 700;
 
-        private CardLayout cardLayout;
+    private CardLayout cardLayout;
+    private LoginView loginView;
+    private LoginDAO loginDAO;
 
-        public MainFrame() {
-                super("West Bank");
+    public MainFrame() {
+        super("West Bank");
 
-                cardLayout = new CardLayout();
+        cardLayout = new CardLayout();
+        loginView = new LoginView();
+        loginDAO = new LoginDAO();
 
-                HomeView home = new HomeView();
-                NavBarView navBar = new NavBarView();
-                LoginView login = new LoginView();
-                TransferView transfer = new TransferView();
-                TransferHistoryView transferHistory = new TransferHistoryView();
+        HomeView home = new HomeView();
+        NavBarView navBar = new NavBarView();
+        TransferirView transferir = new TransferirView();
+        TransferHistoryView transferHistory = new TransferHistoryView();
 
-                new TransferController(transfer);
-                new TransferHistoryController(transferHistory);
+        setLayout(cardLayout);
+        getContentPane().add(loginView, "login");
+        setJMenuBar(navBar);
 
-                setLayout(cardLayout);
-                setJMenuBar(navBar);
-                
-                add(login, "login");
-                add(home, "home");
-                add(transfer, "transfer");
-                add(transferHistory, "transferHistory");
-                
-                navBar.showHome(e -> cardLayout.show(MainFrame.this.getContentPane(), "home"));
-                navBar.showTransferir(e -> cardLayout.show(MainFrame.this.getContentPane(), "transfer"));
-                navBar.showHistorialTransferencias(e -> cardLayout.show(MainFrame.this.getContentPane(), "transferHistory"));
-                
-                ImageIcon imageIcon = new ImageIcon("src/main/java/westbank/assets/favicon.png");
-                setIconImage(imageIcon.getImage());
+        add(loginView, "login");
+        add(home, "home");
+        add(transferir, "transferir");
+        add(transferHistory, "transferHistory");
 
-                setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                setVisible(true);
-        }
+        navBar.showHome(e -> cardLayout.show(MainFrame.this.getContentPane(), "home"));
+        navBar.showTransferir(e -> cardLayout.show(MainFrame.this.getContentPane(), "transferir"));
+        navBar.showHistorialTransferencias(e -> cardLayout.show(MainFrame.this.getContentPane(), "transferHistory"));
+
+        loginView.submitData(e -> {
+            // Obtener el RUN y la clave ingresados por el usuario
+            String run = loginView.getRun();
+            String password = loginView.getPassword();
+
+            // Autenticar al usuario utilizando el LoginDAO
+            Client client = loginDAO.authenticate(run, password);
+
+            if (client != null) {
+                // Autenticación exitosa, mostrar la vista de inicio
+                home.setClientName(client.getName());
+                home.setBalance(client.getAccount().checkBalance());
+                cardLayout.show(MainFrame.this.getContentPane(), "home");
+            } else {
+                // Autenticación fallida, mostrar mensaje de error
+                JOptionPane.showMessageDialog(loginView, "Credenciales inválidas", "Error", JOptionPane.ERROR_MESSAGE);
+                loginView.reset();
+            }
+        });
+
+        navBar.exitApp(e -> System.exit(0));
+
+        ImageIcon imageIcon = new ImageIcon("src/main/java/westbank/assets/favicon.png");
+        setIconImage(imageIcon.getImage());
+
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
